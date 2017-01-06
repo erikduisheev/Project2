fdabb9ca9e77cbd78714b389dd61f1b6ac9cca5d
ErikDuisheev <Duisheev_e@auca.kg>
2017-01-06 18:56:02 +0600

    FinishedGame

---
+++ Duisheev_Tower-Defence/Assets/CompleteLevel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CompleteLevel : MonoBehaviour {
+
+    public string menuSceneName = "MainMenu";
+
+    public string nextLevel = "Level02";
+    public int levelToUnlock = 2;
+
+    public SceneFader sceneFader;
+
+    public void Continue()
+    {
+        PlayerPrefs.SetInt("levelReached", levelToUnlock);
+        sceneFader.FadeTo(nextLevel);
+    }
+
+	public void Menu()
+    {
+        sceneFader.FadeTo(menuSceneName);
+    }
+}
---
+++ Duisheev_Tower-Defence/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public string levelToLoad = "Level_01";
+
+    public SceneFader sceneFader;
+
+	public void Play()
+    {
+        sceneFader.FadeTo(levelToLoad);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Exiting");
+        Application.Quit();
+    }
+}
